b56f102e01890632510a4935202555a696b516e2
OmarRahall <150093843+OmarRahall@users.noreply.github.com>
2023-11-20 11:06:46 -0300

    Create Vilao.h

---
+++ Vilao.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef _VILAO_H_
+#define _VILAO_H_
+#include "Desenhavel.h"
+
+class Vilao : public Desenhavel {
+
+
+public:
+	Vilao(vetor2F pos, vetor2F vel);
+	~Vilao();
+private:
+
+
+};
+
+
+#endif
